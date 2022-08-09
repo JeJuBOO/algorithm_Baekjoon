3989cdae11b4f523127d8cabf7609afa376d43c4
qntjrrud11 <71332005+qntjrrud11@users.noreply.github.com>
2022-08-09 23:55:34 +0900

    [ 22.08.09 23:55 ] Beakjoon 10828 정답

--- algorithm_basic_1/bj10828_stack.py
+++ algorithm_basic_1/bj10828_stack.py
@@ -2,31 +2,42 @@
 # BOO.SK 
 
 # Algorithm basic 1/2 in Baekjoon
+# 1차시 : 시간 초과로 size 함수제거하고 사이즈를 변수로 저장
+# 2차시 : input() 함수의 시간이 오래걸린다 하여 sys.stdin.readline()로 변경
 
-N = int(input()) # 처음 명령은 총 명령의 수를 입력받음
+import sys
+
+N = int(sys.stdin.readline()) # 처음 명령은 총 명령의 수를 입력받음
 stack = []
+stack_size = 0
 
 for _ in range(N):
-    command = input().split()
+    command = sys.stdin.readline().split()
 
     if command[0] == 'push':
         stack.append(int(command[1]))
+        stack_size += 1
+
     elif command[0] == 'pop':
-        if stack == []:
+        if stack_size == 0:
             print(-1)
         else:
             x = stack.pop()
+            stack_size -= 1
             print(x)
+
     elif command[0] == 'size':
-        print(len(stack))
+        print(stack_size)
+
     elif command[0] == 'empty':
-        if stack == []:
+        if stack_size == 0:
             print(1)
         else:
             print(0)
+
     else:
-        if stack == []:
+        if stack_size == 0:
             print(-1)
         else:
-            print(stack[len(stack)-1])
+            print(stack[stack_size-1])
 
